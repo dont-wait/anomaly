8da8eae0485faf1869d4f705937878edef0b60b3
dont-wait <nguyentansang15042005@gmail.com>
2026-08-18 05:13:40 +0700

    again

--- server/internal/application/account/commands/id.go
+++ server/internal/application/account/commands/id.go
@@ -3,10 +3,17 @@ package commands
 import (
 	"crypto/rand"
 	"encoding/hex"
+
+	"github.com/dont-wait/anomaly/internal/logger"
+	"github.com/rs/zerolog"
 )
 
 func newID() string {
+	logger := logger.NewLogger(zerolog.Level(zerolog.InfoLevel))
 	b := make([]byte, 16)
-	rand.Read(b)
+	_, err := rand.Read(b)
+	if err != nil {
+		logger.Info().Msgf("err: %v", err)
+	}
 	return hex.EncodeToString(b)
 }
