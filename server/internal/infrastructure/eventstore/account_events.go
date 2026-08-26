8b91e7b4f9d792d506aa6d5efd6611db31f1498c
sang-nt <157901375+dont-wait@users.noreply.github.com>
2026-08-26 10:32:48 +0700

    typo

--- server/internal/infrastructure/eventstore/account_events.go
+++ server/internal/infrastructure/eventstore/account_events.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	EventAccountCreated  = "AccountCreated"
-	EventAccountWithdraw = "AccountWithdrew"
+	EventAccountWithdraw = "AccountWithdraw"
 )
 
 type accountCreatedPayload struct {
