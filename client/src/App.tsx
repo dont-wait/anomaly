258ccc9ddc5dfd77acc94fa62d7e4aae617b86b7
dont-wait <nguyentansang15042005@gmail.com>
2026-09-11 22:15:31 +0700

    clean

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,3 @@
-// import { LoginPage } from "@/pages/LoginPage";
-// import DashboardPage from "./pages/DashboardPage";
-
-// const App = () => {
-//   return <DashboardPage />;
-// };
-// export default App;
 import { AppRouter } from "@/app/AppRouter";
 
 const App = () => <AppRouter />;
